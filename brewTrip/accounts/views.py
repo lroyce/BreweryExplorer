23184630cef93e04f297311d92063a83f8f240fe
Laura Royce <laura.a.royce@disney.com>
2018-06-18 11:09:52 -0700

    added login logout signup

--- brewTrip/accounts/views.py
+++ brewTrip/accounts/views.py
@@ -1,10 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from .forms import SignUpForm
 
 def signup(request):
-    return render(request,'signup.html',{'title':'Signup'})
+    registered = False
+    if request.method == 'POST':
+        signup_form = SignUpForm(data=request.POST)
+        if signup_form.is_valid():
+            user = signup_form.save()
+            user.set_password(user.password)
+            user.save()
+            registered = True
+    else:
+        signup_form = SignUpForm()
+    return render(request, 'signup.html',{
+            'title':'Sign up Below',
+            'signup_form':signup_form,
+            'registered': registered,
+            })
+
 
 def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(
+        username=request.POST['username'],
+        password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html',{
+            'title':'Something Went Wrong',
+            'error':'Username or Password Invalid'
+            })
     return render(request, 'login.html',{'title':'Please Login'})
 
+
 def logout(request):
-    pass
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
