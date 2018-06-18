92a9391fe5b3ecc0bf243700d68aca9bff8b91c6
Laura Royce <laura.a.royce@disney.com>
2018-06-18 09:46:31 -0700

    working base template

--- brewTrip/accounts/models.py
+++ brewTrip/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class BrewTrips(models.Model):
+    brewery_name = models.CharField(max_length = 100)
+    brewery_address = models.CharField(max_length = 100)
+    brewery_url = models.CharField(max_length = 40)
+    latitude = models.CharField(max_length = 20)
+    longitude = models.CharField(max_length = 20)
+    added_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+    def prettify_datetime(self):
+        return self.added_date.strftime('%b %b %y')
