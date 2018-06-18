e568a6b46a4c9d0fcd3c5cddd4fac8ad40650e25
Laura Royce <laura.a.royce@disney.com>
2018-06-18 16:31:06 -0700

    staging files and tsc

---
+++ brewTrip/static/build/foo.js
@@ -0,0 +1,8 @@
+"use strict";
+var add = function (a, b) {
+    var result = a + b;
+    console.log(a + " + " + b + " = " + result);
+    return result;
+};
+add(1, 2);
+console.log(add(4, 5));
---
+++ brewTrip/ts/foo.ts
@@ -0,0 +1,7 @@
+const add = (a: number, b: number): number => {
+  const result = a + b;
+  console.log(`${a} + ${b} = ${result}`);
+  return result;
+};
+add(1, 2);
+add(4, 5);
