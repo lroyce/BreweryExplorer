687c25da7795944000a1485e2e75e12db9f3bc67
Laura Royce <laura.a.royce@disney.com>
2018-06-19 10:10:10 -0700

    post is working

--- brew_trips/urls.py
+++ brew_trips/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
 path('',views.home,name='home'),
-path('create/',views.create,name='create'),
-path('brewmap/',views.brewMap,name='brewMap'),
+# path('create/',views.create,name='create'),
+path('brewmap/',views.brewmap,name='brewmap'),
 path('<int:brewery_id>/delete/',views.delete, name='delete'),
 
 # path('update/',views.update,name='update'),
--- brew_trips/views.py
+++ brew_trips/views.py
@@ -13,7 +13,29 @@ def home(request):
     }
     return render(request, 'home.html', context)
 
-# @login_required
+@login_required
+def brewmap(request):
+    key = settings.BEER_MAP_KEY
+    locations = {}
+    if key:
+        call = requests.get(f"http://beermapping.com/webservice/loccity/{key}/portland,or&s=json")
+        locations = call.json()
+        # print(locations[0])
+        if request.method == 'POST':
+            plan_form = BrewForm(data=request.POST)
+            if plan_form.is_valid():
+                plan = plan_form.save(commit=False)
+                plan.brew_user = request.user
+                plan.save()
+                return redirect('home')
+        else:
+            plan_form = BrewForm()
+
+        return render(request, 'brewmap.html', {
+        'locations':locations,
+
+        })
+#
 # def create(request):
 #     if request.method == 'POST':
 #         plan_form = BrewForm(data=request.POST)
@@ -25,26 +47,26 @@ def home(request):
 #     else:
 #         plan_form = BrewForm()
 #
-#     return render(request, 'createPlan.html', {
+#     return render(request, 'create.html', {
 #     'title':'Add a new trip',
 #     'plan_form': plan_form
 #     })
 
-@login_required
-def create(request):
-    if request.method == 'POST':
-        locations = BrewForm(data=request.POST)
-        if locations.is_valid():
-            plan = locations.save(commit=False)
-            plan.brew_user = request.user
-            plan.save()
-            return redirect('home')
-    else:
-        locations = BrewForm()
-    return render(request, 'createPlan.html', {
-    'title':'Add a new trip',
-    'locations': locations
-    })
+# @login_required
+# def create(request):
+#     if request.method == 'POST':
+#         locations = BrewForm(data=request.POST)
+#         if locations.is_valid():
+#             plan = locations.save(commit=False)
+#             plan.brew_user = request.user
+#             plan.save()
+#             return redirect('home')
+#     else:
+#         locations = BrewForm()
+#     return render(request, 'create.html', {
+#     'title':'Add a new trip',
+#     'locations': locations
+#     })
 
 
 @login_required
@@ -55,11 +77,11 @@ def delete(request, plan_id):
         return redirect('home', user_id=request.user.id)
 
 
-def brewMap(request):
-    key = settings.BEER_MAP_KEY
-    locations = {}
-    if key:
-        call = requests.get(f"http://beermapping.com/webservice/loccity/{key}/eugene,or&s=json")
-        locations = call.json()
-        print(locations[0])
-    return render(request, 'brewMap.html', {'locations':locations})
+# def brewMap(request):
+#     key = settings.BEER_MAP_KEY
+#     locations = {}
+#     if key:
+#         call = requests.get(f"http://beermapping.com/webservice/loccity/{key}/eugene,or&s=json")
+#         locations = call.json()
+#         print(locations[0])
+#     return render(request, 'create.html', {'locations':locations})
