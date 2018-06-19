fb1b8595ddd3bfd6a62a9303eb018a4a4d68980f
Laura Royce <laura.a.royce@disney.com>
2018-06-19 12:14:39 -0700

    delete post is working

--- brew_trips/urls.py
+++ brew_trips/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
 path('',views.home,name='home'),
 # path('create/',views.create,name='create'),
 path('brewmap/',views.brewmap,name='brewmap'),
-path('<int:brewery_id>/delete/',views.delete, name='delete'),
+path('<int:id>/delete/',views.delete, name='delete'),
 
 # path('update/',views.update,name='update'),
 ]
--- brew_trips/views.py
+++ brew_trips/views.py
@@ -9,7 +9,7 @@ import requests
 def home(request):
     context = {
     'title':'Welcome To Your Trips',
-    'breweries': BrewTrips.objects.order_by('added_date'),
+    'breweries': BrewTrips.objects.order_by('-added_date'),
     }
     return render(request, 'home.html', context)
 
@@ -20,7 +20,6 @@ def brewmap(request):
     if key:
         call = requests.get(f"http://beermapping.com/webservice/loccity/{key}/portland,or&s=json")
         locations = call.json()
-        # print(locations[0])
         if request.method == 'POST':
             plan_form = BrewForm(data=request.POST)
             if plan_form.is_valid():
@@ -33,7 +32,6 @@ def brewmap(request):
 
         return render(request, 'brewmap.html', {
         'locations':locations,
-
         })
 #
 # def create(request):
@@ -52,29 +50,12 @@ def brewmap(request):
 #     'plan_form': plan_form
 #     })
 
-# @login_required
-# def create(request):
-#     if request.method == 'POST':
-#         locations = BrewForm(data=request.POST)
-#         if locations.is_valid():
-#             plan = locations.save(commit=False)
-#             plan.brew_user = request.user
-#             plan.save()
-#             return redirect('home')
-#     else:
-#         locations = BrewForm()
-#     return render(request, 'create.html', {
-#     'title':'Add a new trip',
-#     'locations': locations
-#     })
-
-
 @login_required
-def delete(request, plan_id):
+def delete(request,id):
     if request.method == 'POST':
-        plan = get_object_or_404(BrewTrips, pk=plan_id)
+        plan = get_object_or_404(BrewTrips,pk=id)
         plan.delete()
-        return redirect('home', user_id=request.user.id)
+        return redirect('home')
 
 
 # def brewMap(request):
