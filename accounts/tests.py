73390511f6f63e10fef478a3d4f0683ad0aacd41
Laura Royce <laura.a.royce@disney.com>
2018-06-21 20:47:19 -0700

    adding tests for accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import BrewTrips
 
-# Create your tests here.
+
+class BrewTest(TestCase):
+    def setUp(self):
+        self.user = User(username="macey")
+        self.user.save()
+        self.client = Client()
+
+    def test_getPath(self):
+        self.client.get('/login/', {'username': 'macey', 'bar': 2})
+        self.client.post('/signup/', {'name': 'stacey'}, follow=True)
--- static/js/cityForm.js
+++ static/js/cityForm.js
@@ -1,20 +1,15 @@
-const $form = $('#my-form');
-const $favorite = $('#favorite').val();
 $(document).ready(function(){
+
 ////////http://jsfiddle.net/kbwood/DLprk/////////
 //for loading iframe on button click ///////////
   $('#load').click(function() {
     $('#display').attr('src', $('#url').val());
   });
+  // $load.click(function() {
+  //   $('#display').attr('src', $('#url').val());
+  // });
 
 
 
-  $('#favorite').click(function() {
 
-    // if ($favorite.val() === 'False') {
-    //   $favorite.val('Not Starred')
-
-
-  })
-  
 })
