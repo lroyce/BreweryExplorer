b302e1771a73222f1410fb362ba41bae7ac08748
Laura Royce <laura.a.royce@disney.com>
2018-06-21 21:20:27 -0700

    testing accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,10 +1,11 @@
-from django.test import TestCase, Client
+from django.test import TestCase, Client, override_settings
 from django.urls import reverse
 from django.contrib.auth.models import User
-from .models import BrewTrips
+from io import BytesIO
+# from ..models import BrewTrips
 
 
-class BrewTest(TestCase):
+class AccountsTest(TestCase):
     def setUp(self):
         self.user = User(username="macey")
         self.user.save()
@@ -13,3 +14,18 @@ class BrewTest(TestCase):
     def test_getPath(self):
         self.client.get('/login/', {'username': 'macey', 'bar': 2})
         self.client.post('/signup/', {'name': 'stacey'}, follow=True)
+
+    def test_get(self):
+        self.client.get('', {'foo': 1, 'bar': 2})
+
+    def test_index(self):
+        client = Client()
+        response = client.get('/signup/')
+        self.assertEqual(response.status_code, 404)
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 404)
+
+    @override_settings(LOGIN_URL='/login/')
+    def test_login(self):
+        response = self.client.get('/sekrit/')
+        self.assertRedirects(response, '/login/?next=/sekrit/')
