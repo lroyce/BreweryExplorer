4a6c889c937378f03494f4ee338120064aca66bc
Laura Royce <laura.a.royce@disney.com>
2018-06-22 10:28:00 -0700

    cleaning up code

--- static/js/cityForm.js
+++ static/js/cityForm.js
@@ -2,24 +2,12 @@ $(document).ready(function(){
 
 ////////http://jsfiddle.net/kbwood/DLprk/////////
 //for loading iframe on button click ///////////
-  // $('#load').click(function() {
-  //   $('#display').attr('src', $('#url').val());
-  // });
 
-//class on all the buttons
-//grab the id of the url
-//and then fill the iframe src
 
 $('.form-button-map').click(function() {
     const a = $(this).attr("id");
     $('#display').attr('src', (a));
   });
-
-
-  // $load.click(function() {
-  //   $('#display').attr('src', $('#url').val());
-  // });
-
 
 
 
